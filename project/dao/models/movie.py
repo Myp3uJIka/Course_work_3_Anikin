3adfb69f426fe102e70a4c473e0d4a0aadfc22a7
Мефодий <storage@list.ru>
2022-04-25 05:37:12 +1000

    Добавлены новые модели в проект.

---
+++ project/dao/models/movie.py
@@ -0,0 +1,19 @@
+from project.dao.models.base import BaseMixin
+from project.setup_db import db
+
+
+class Movie(BaseMixin, db.Model):
+    __tablename__ = 'movies'
+
+    title = db.Column(db.String(100), unique=True, nullable=False)
+    description = db.Column(db.String(255))
+    trailer = db.Column(db.String(255))
+    year = db.Column(db.Integer)
+    rating = db.Column(db.Float)
+    genre_id = db.Column(db.Integer, db.ForeignKey("genres.id"))
+    genres = db.relationship("Genre")
+    director_id = db.Column(db.Integer, db.ForeignKey("directors.id"))
+    genres = db.relationship("Director")
+
+    def __repr__(self):
+        return f"<Movie '{self.title.title()}'>"
