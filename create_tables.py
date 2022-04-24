89abfe9b333aeb71baca2fb5407f5b7844908027
Мефодий <storage@list.ru>
2022-04-25 05:36:46 +1000

    "from project.dao.models import *" работало некорректно. Строчка удалена, заменена добавлением каждой модели в отдельности.

--- create_tables.py
+++ create_tables.py
@@ -1,5 +1,8 @@
 from project.config import DevelopmentConfig
-from project.dao.models import *  # noqa F401, F403
+from project.dao.models import Genre
+from project.dao.models.director import Director
+from project.dao.models.movie import Movie
+from project.dao.models.user import User
 from project.server import create_app
 from project.setup_db import db
 
@@ -8,3 +11,5 @@ app = create_app(DevelopmentConfig)
 with app.app_context():
     db.drop_all()
     db.create_all()
+
+
