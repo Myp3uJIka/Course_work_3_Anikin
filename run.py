7d42f21a9adce688e5251187f2328de0b852241c
Мефодий <storage@list.ru>
2022-04-25 07:34:59 +1000

    Расширен @app.shell_context_processor, и добавлены параметры запуска для docker (указан порт).

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from project.config import DevelopmentConfig
-from project.dao.models import Genre
+from project.dao.models import *
 from project.server import create_app, db
 
 app = create_app(DevelopmentConfig)
@@ -10,4 +10,10 @@ def shell():
     return {
         "db": db,
         "Genre": Genre,
+        "Movie": Movie,
+        "Director": Director,
+        "User": User
     }
+
+
+app.run(host="localhost", port=5000, debug=True)
