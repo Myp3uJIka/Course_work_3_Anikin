4698fcf81448e89bc783413ea91ff5574c1c2381
Мефодий <storage@list.ru>
2022-04-27 02:15:38 +1000

    Добавлена ссылка на нэймспейс "auth".

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -1,3 +1,4 @@
+from .auth import auth_ns
 from .directors import directors_ns
 from .genres import genres_ns
 from .movies import movies_ns
@@ -5,5 +6,6 @@ from .movies import movies_ns
 __all__ = [
     "genres_ns",
     "movies_ns",
-    "directors_ns"
+    "directors_ns",
+    "auth_ns"
 ]
