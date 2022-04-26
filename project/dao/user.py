bd8035504dd7fc9a80c8b56dd1f9196ce2769ad4
Мефодий <storage@list.ru>
2022-04-27 02:21:53 +1000

    Написан DAO для пользователя.

--- project/dao/user.py
+++ project/dao/user.py
@@ -7,3 +7,35 @@ class UserDAO:
     def __init__(self, session: scoped_session):
         self._db_session = session
 
+    def get_one(self, uid):
+        return self._db_session.query(User).get(uid)
+
+    def get_by_email_password(self, user):
+        return self._db_session.query(User).filter(
+            User.email == user['email'],
+            User.password == user['password']
+        ).one()
+
+    def get_all(self):
+        return self._db_session.query(User).all()
+
+    def create(self, new_user):
+        user = User(**new_user)
+        self._db_session.add(user)
+        self._db_session.commit()
+
+    def delete(self, uid):
+        user = self.get_one(uid)
+        self._db_session.delete(user)
+        self.commit()
+
+    def update(self, user_update):
+        user = self._db_session.query(User).filter(User.email == user_update['email']).one()
+        user.id = user_update['id']
+        user.email = user_update['email']
+        user.name = user_update['name']
+        user.surname = user_update['surname']
+        user.favorite_genre = user_update['favorite_genre']
+        user.role = user_update['role']
+
+        self._db_session.commit()
