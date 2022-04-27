b4fb1f7b09dcbf0a895cfff1756ce52da52c36c6
Мефодий <storage@list.ru>
2022-04-27 10:15:52 +1000

    Добавлена инициализация нэймспейса пользователей.

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -2,10 +2,12 @@ from .auth import auth_ns
 from .directors import directors_ns
 from .genres import genres_ns
 from .movies import movies_ns
+from .users import users_ns
 
 __all__ = [
     "genres_ns",
     "movies_ns",
     "directors_ns",
-    "auth_ns"
+    "auth_ns",
+    "users_ns"
 ]
