a4d36939b6883c49515018f3764c3afd739756c5
Мефодий <storage@list.ru>
2022-04-27 10:21:21 +1000

    Добавлены сервисы для /user.

--- project/services/users_service.py
+++ project/services/users_service.py
@@ -1,9 +1,20 @@
+from flask import current_app
+
 from project.dao import UserDAO
+from project.schemas.user import UserSchema
 from project.services.base import BaseService
 from project.tools.security import generate_password_digest
 
 
 class UsersService(BaseService):
+    def get_all(self):
+        users = UserDAO(self._db_session).get_all()
+        return UserSchema(many=True).dump(users)
+
+    def get_one(self, u_id):
+        user = UserDAO(self._db_session).get_one(u_id)
+        return UserSchema().dump(user)
+
     def get_by_email_password(self, data):
         data['password'] = generate_password_digest(data['password'])
         return UserDAO(self._db_session).get_by_email_password(data)
@@ -12,3 +23,32 @@ class UsersService(BaseService):
         data['role'] = 'user'
         data['password'] = generate_password_digest(data['password'])
         return UserDAO(self._db_session).create(data)
+
+    def update(self, data):
+        user = UserDAO(self._db_session).get_one(data['id'])
+        if 'email' in data:
+            user.email = data['email']
+        if 'password' in data:
+            user.password = data['password']
+        if 'name' in data:
+            user.name = data['name']
+        if 'surname' in data:
+            user.surname = data['surname']
+        if 'favourite_genre' in data:
+            user.favorite_genre = data['favourite_genre']
+        if 'role' in data:
+            user.role = data['role']
+        UserDAO(self._db_session).update(user)
+
+    def get_user_password(self, u_id):
+        user = UserDAO(self._db_session).get_one(u_id)
+        return user.password
+
+    # def get_limit_users(self, page):
+    #     limit = current_app.config['ITEMS_PER_PAGE']
+    #     offset = (page - 1) * limit
+    #     users = UserDAO(self._db_session).get_limit(limit=limit, offset=offset)
+    #     return UserSchema(many=True).dump(users)
+
+    # def get_all_users(self):
+    #     users = UserDAO(self._db_session).get_all()
